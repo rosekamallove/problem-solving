94e5b26bddf5e2b4af3ba73555dd7dbe1f90b882
rosekamallove <rosekamallove@gmail.com>
2021-07-21 12:42:08 +0530

    -

---
+++ Practice/CF/C-Pursuit.cpp
@@ -0,0 +1,136 @@
+/* 
+ * WebSite: https://rosekamallove.github.io/rosek
+ * GitHub : https://github.com/rosekamallove
+ */
+
+#include "bits/stdc++.h"
+using namespace std;
+
+#define int long long
+#define endl "\n"
+#define all(x) x.begin(),x.end()
+
+#ifndef ONLINE_JUDGE
+#define deb(x) cerr<<#x<<"=>"; _print(x); cerr<<endl;
+#else
+#define deb(x)
+#endif
+
+int T(1);
+void fastIO();
+template<typename T>        void _print(T n);
+template <class T, class V> void _print(pair <T, V> p);
+template <class T>          void _print(vector <T> v);
+template <class T>          void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T>          void _print(multiset <T> v);
+template<typename... T>     void read(T&... args);
+template<typename... T>     void write(string delimiter, T &&...args);
+template<typename T>        void readContainer(T &t);
+template<typename T>        void writeContainer(string delimiter, T &t);
+//----------------------------------------------------------------------------------------------------//
+
+void testCase() {
+    int n; read(n);
+    vector<int> me(n), ily(n);
+    readContainer(me), readContainer(ily);
+}
+
+
+signed main() {
+    /*--------------------------------------*/
+    fastIO();
+#ifndef ONLINE_JUDGE
+    freopen(".deb.txt", "w", stderr);
+#endif
+    /*--------------------------------------*/
+    cin >> T;
+    while (T--) {
+        cerr<<"<------TC------>"<<endl;
+        testCase();
+    }
+    cerr << "Time : " << 1000 * ((double)clock()) / (double)CLOCKS_PER_SEC << "ms\n";
+}
+
+
+//----------------------------------------------------------------------------------------------------//
+template <typename... T>
+void read(T &...args) {
+    ((cin >> args), ...);
+}
+
+template <typename... T>
+void write(string delimiter, T &&...args) {
+    ((cout << args << delimiter), ...);
+}
+
+template <typename T>
+void readContainer(T &t) {
+    for (auto &e : t) {
+        read(e);
+    }
+}
+
+template <typename T>
+void writeContainer(string delimiter, T &t) {
+    for (const auto &e : t) {
+        write(delimiter, e);
+    }
+    write("\n");
+}
+
+template<typename T>
+void _print(T n) {
+    cerr << n;
+}
+
+template<typename T>
+void _print(vector<T> v) {
+    cerr << "[ ";
+    for (auto val : v) {
+        cerr << val;
+        cerr << ' ';
+    }
+    cerr << "]";
+}
+template <class T, class V>
+void _print(pair <T, V> p) {
+    cerr << "{";
+    _print(p.first);
+    cerr << ",";
+    _print(p.second);
+    cerr << "}";
+}
+
+template <class T>
+void _print(set <T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T> void _print(multiset <T> v) {
+    cerr << "[ ";
+    for (T i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class V> void _print(map <T, V> v) {
+    cerr << "[ ";
+    for (auto i : v) {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+void fastIO() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+}
+
--- Practice/CF/crap.cpp
+++ Practice/CF/crap.cpp
@@ -1,13 +1,52 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define int long long
+#define endl "\n"
+#define deb(x) cout << #x << ": " << x << endl;
+#define tdeb(x, y) cout << #x << ": " << x << #y << ": " << y << endl;
+#define rep(i, n) for (int i(0); i < n; i++)
+#define rep2(j, n) for (int j(0); j < n; j++)
+#define rip(i, n) for (int i(1); i <= n; i++)
+#define rap(i, a, n) for (int i(a); i < n; i++)
 
-int main(){
-    int cnt(0);
-    for(int i=1; i<=200; i++){
-        if(i%2==0){
-            cnt++;
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if ((i == 0 or i == n - 1) and j % 2 == 0) {
+                arr[i][j] = 1;
+            }
+            else{
+                arr[i][j] = 0;
+            }
         }
     }
-    cout<<cnt<<endl;
+    for (int i = 1; i < n - 1; i++) {
+        if (i % 2 == 0 and i != n - 2) {
+            arr[i][0] = 1;
+            arr[i][m - 1] = 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << arr[i][j];
+        }
+        cout << endl;
+    }
+}
+
+signed main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
 }
 
