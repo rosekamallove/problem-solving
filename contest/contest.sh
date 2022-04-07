9bcf356eb7bf0d98d4827df8f81d8036500fb36b
Rose Kamal Love <rosekamallove@gmail.com>
2022-04-07 16:05:49 +0530

    re-design;

--- contest/contest.sh
+++
@@ -1,7 +0,0 @@
-mkdir ~/Documents/problem-solving/contests/$1
-cd ~/Documents/problem-solving/contests/$1
-touch {A..D}.cpp
-cat ~/.vim/templates/code.cpp >> {A..D}.cpp
-touch .zin.txt .zot.txt .zdf.txt .zex.txt .deb.txt
-cp ~/Contest/.cp.vim ./
-vim -S .cp.vim
